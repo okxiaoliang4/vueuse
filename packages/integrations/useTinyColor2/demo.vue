b8cad394fdbc3f3d2a5e13482b1089fd03039984
okxiaoliang4 <353742991@qq.com>
2021-12-10 18:50:38 +0800

    feat(useTinyColor2): computed property

--- packages/integrations/useTinyColor2/demo.vue
+++ packages/integrations/useTinyColor2/demo.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
 import { useTinyColor2 } from '.'
 
-const { rgb } = useTinyColor2('#f00')
+const { rgb, rgba, hsl, hsla, hsv, hex, hex8, isLight } = useTinyColor2('#fff00055')
 
 </script>
 
 <template>
-  <div>rgb:{{ rgb }}</div>
+  <div>rgb: {{ rgb }}</div>
+  <div>rgba: {{ rgba }}</div>
+  <div>hsl: {{ hsl }}</div>
+  <div>hsla: {{ hsla }}</div>
+  <div>hsv: {{ hsv }}</div>
+  <div>hex: {{ hex }}</div>
+  <div>hex8: {{ hex8 }}</div>
+  <div>isLight: {{ isLight }}</div>
 </template>
--- packages/integrations/useTinyColor2/index.ts
+++ packages/integrations/useTinyColor2/index.ts
@@ -1,11 +1,24 @@
-import { ref } from 'vue-demi'
+import type { ColorInput } from 'tinycolor2'
 
-export function useTinyColor2() {
-  const rgb = ref('')
-  const rgba = ref('')
-  const hsl = ref('')
-  const hsla = ref('')
-  const hsv = ref('')
+import { MaybeRef } from '@vueuse/shared'
+import { computed, ref, unref, watch } from 'vue-demi'
+import tinycolor from 'tinycolor2'
+
+export function useTinyColor2(input: MaybeRef<ColorInput>) {
+  const tinycolorRef = ref(tinycolor(unref(input)))
+
+  const rgb = computed(() => tinycolorRef.value.toRgbString())
+  const rgba = computed(() => tinycolorRef.value.toRgbString())
+  const hsl = computed(() => tinycolorRef.value.toHslString())
+  const hsla = computed(() => tinycolorRef.value.toHslString())
+  const hsv = computed(() => tinycolorRef.value.toHsvString())
+  const hex = computed(() => `#${tinycolorRef.value.toHex()}`)
+  const hex8 = computed(() => `#${tinycolorRef.value.toHex8()}`)
+  const isLight = computed(() => tinycolorRef.value.isLight())
+
+  watch(() => unref(input), (newInput) => {
+    tinycolorRef.value = tinycolor(newInput)
+  })
 
   return {
     rgb,
@@ -13,5 +26,8 @@ export function useTinyColor2() {
     hsl,
     hsla,
     hsv,
+    hex,
+    hex8,
+    isLight,
   }
 }
